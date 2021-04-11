407e9b7cfd4f640cdc1e1745bdd756b99a99288e
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-04-11 17:11:54 +0300

    QT imajları düzeltildi.

---
+++ q/qt6/docker_start.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Fix for running GUI programs from within the docker container:
+#
+# Make xorg disable access control, i.e. let any x client connect to our
+# server.
+xhost +
+
+docker start qt-sh --attach --interactive
+docker start qtcreator --attach --interactive
